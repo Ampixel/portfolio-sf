b6d90079e0a0697f18547f2373846c16e9b43b42
amandine.monardo <amandine.monardo@web-atrio.com>
2019-07-05 07:58:19 +0200

    [AMM] avancement

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Entity\Activite;
+use App\Entity\Experience;
+use App\Entity\Formation;
 use App\Form\ActiviteType;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController;
@@ -9,18 +11,38 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ActiviteController extends AdminController
 {
-    public function new(Request $request, EntityManagerInterface $entityManager)
+    public $em;
+    function  __construct(EntityManagerInterface $em)
     {
-        $activite = new Activite();
-        $form = $this->createForm(ActiviteType::class, $activite);
+        $this->em = $em;
+    }
+
+    public function new(Request $request)
+    {
+        /*
+         * @var Experience $experience
+         */
+        $experience = $this->em->getRepository(Experience::class)->findAll();
+        //On récupère l'expérience
+
+        /*
+         * @var Formation $formation
+         */
+        $formation = $this->em->getRepository(Formation::class)->findAll();
+        //on récupère la formation
+
+        $experience->getId();
+        $formation->getId(); //on récupère l'id
+        $activite = new Activite(); // On initie une nouvelle entité activité
+        $form = $this->createForm(ActiviteType::class, $activite);//on créer un form qui prend la class activiteType
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
 
-            $entityManager->persist($activite);
-            $entityManager->flush();
+            $this->em->persist($activite);//on persist notre activite (garde cette entitée en mémoire)
+            $this->em->flush();//Ouvre une transaction et enregistre toutes les entités
         }
 
-        return $this->render('easy_admin', ['form' => $form->createView()]);
+        return $this->render('easy_admin.yaml', ['form' => $form->createView()]);
     }
 }
